54996268448af44e571101855c78ec5971bfed27
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-09-14 18:06:28 -0600

    add imports, some questions, and wrote most of init()

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+  },
+  {
+    type: "input",
+    name: "description",
+  },
+  {
+    type: "input",
+    name: "usage",
+  },
+  {
+    type: "input",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    name: "credits",
+  },
+  {
+    type: "input",
+    name: "github",
+  },
+  {
+    type: "input",
+    name: "email",
+  },
+  {
+    type: "list",
+    name: "license",
+    choices: ["MIT", "none"],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  const userData = await inquirer.prompt(questions);
+  const markdown = generateMarkdown(userData);
+
+  const targetPath = process.argv[2] || "./output/";
+  const filename = "README.md";
+  writeToFile(targetPath + filename, markdown);
+}
 
 // Function call to initialize app
 init();
