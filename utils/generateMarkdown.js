abee776892f723bb134d756d2de0f378e4262de7
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-09-14 18:14:14 -0600

    add template markdown string to generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +14,54 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${title}
 
+${badge}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+[Installation](#installation)
+[Usage](#usage)
+[Testing](#testing)
+[Contributing](#contributing)
+[Credits](#credits)
+[License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Testing
+
+${testing}
+
+## Contributing
+
+${contributing}
+
+## Questions
+
+You can reach me:
+
+- on GitHub [${githubName}](${githubURL})
+- via email: ${email}
+
+## Credits
+
+${credits}
+
+## [License - ${licenseName}](${licenseURL})
+
+${licenseBody}
 `;
 }
 
